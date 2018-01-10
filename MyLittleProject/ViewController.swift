2d84b0efeccb90df15a9b23df18348641b076b85
Bastien <bcecillon@vision-systems.fr>
2018-01-10 17:42:38 +0100

    first commit

---
+++ MyLittleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittleProject
+//
+//  Created by Vision Systems on 10/01/2018.
+//  Copyright © 2018 Vision Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
